bf236ae4e0f5fea6a7fa0b1419631619b9a2502e
Aviv Pilipski <apilipski1@hwemail.com>
2024-09-17 13:31:39 -0700

    Initialized new git Repo without edge case & tester!

--- Git.java
+++ Git.java
@@ -1,6 +1,21 @@
+import java.io.File;
+
 public class Git {
+    public Git () {
+        File gitDirectory = new File ("git");
+        if (!gitDirectory.exists()) {
+            gitDirectory.mkdir();
+            File objectsDirectory = new File ("git/objects");
+            objectsDirectory.mkdirs();
+            File indexFile = new File ("git/index");
+            indexFile.mkdirs();
+
+        }
+    }
+    
     public static void main (String [] args)
     {
-        
+        Git newGit = new Git();
+        System.out.println ("Made a Git!");
     }
 }
