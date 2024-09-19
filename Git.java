6a50108b875d810dfd3d8e517b2e8b118718c9d2
Aviv Pilipski <apilipski1@hwemail.com>
2024-09-19 00:21:38 -0400

    Milestone #2 Outline Code

--- Git.java
+++ Git.java
@@ -1,4 +1,10 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Git {
     public Git () {
@@ -24,4 +30,26 @@ public class Git {
             System.out.println("Git Repository already exists");
         }
     }
+    
+    public String hashingFunction (String fileName) throws FileNotFoundException {
+        File file = new File (fileName);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        return "";
+    }
+    public void fileWriter (String fileName) throws IOException {
+        String hash = hashingFunction(fileName);
+        File objectsDirectory = new File ("git/objects/hash");
+        objectsDirectory.mkdirs();
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        BufferedWriter bw = new BufferedWriter(new FileWriter("git/objects/hash"));
+        while (br.ready()) {
+            bw.write(br.read());
+        }
+        
+    }
+
+    public void makeBLOB (String fileName) throws FileNotFoundException {
+        fileWriter(fileName);
+    }
+
 }
