97cb93842b73f96e99ea57e9910f1adba5a508eb
Aviv Pilipski <apilipski1@hwemail.com>
2024-09-22 00:43:52 -0700

    Fixed Git Initialization Tester
    It was making a index directory instead of an index file!

--- initTester.java
+++ initTester.java
@@ -1,51 +1,52 @@
 import java.io.File;
+import java.io.IOException;
 
 public class initTester {
     public static void deleteCurrent () {
         File gitDirectory = new File ("git");
         File objectsDirectory = new File ("git/objects");
-        File indexFile = new File ("git/index");
+        String indexFileName = "index";
+        File indexFile = new File (gitDirectory, indexFileName);
         if (gitDirectory.exists() && objectsDirectory.exists() && indexFile.exists()) {
             gitDirectory.delete();
-            objectsDirectory.delete();
-            indexFile.delete();
-            System.out.println("Git Directory, Objects Directory, and Index File DELETED.");
+            System.out.println("Git Directory, Objects Directory, and Index File ALL EXISTED.");
         }
         else if (gitDirectory.exists() && objectsDirectory.exists() && !indexFile.exists()) {
-            gitDirectory.delete();
             objectsDirectory.delete();
-            System.out.println("Git Directory, and Objects Directory DELETED. Index File DID NOT EXIST.");
+            gitDirectory.delete();
+            System.out.println("Git Directory, and Objects Directory EXISTED. Index File DID NOT EXIST.");
         }
         else if (gitDirectory.exists() && !objectsDirectory.exists() && indexFile.exists()) {
             gitDirectory.delete();
-            indexFile.delete();
-            System.out.println("Git Directory, and Index File DELETED. Objects Directory DID NOT EXIST.");
+            System.out.println("Git Directory, and Index File EXISTED. Objects Directory DID NOT EXIST.");
         }
         else if (gitDirectory.exists() && !objectsDirectory.exists() && !indexFile.exists()) {
             gitDirectory.delete();
-            System.out.println("Git Directory DELETED. Objects Directory and Index File DID NOT EXIST.");
+            System.out.println("Git Directory EXISTED. Objects Directory and Index File DID NOT EXIST.");
         }
         else {
             System.out.println("Git Directory, Objects Directory, and Index File ALL DID NOT EXIST");
         }
 
     }
-    public static void main (String [] args) {
+    public static void main (String [] args) throws IOException {
         File gitDirectory = new File ("git");
         File objectsDirectory = new File ("git/objects");
-        File indexFile = new File ("git/index");
+        String indexFileName = "index";
+        File indexFile = new File (gitDirectory, indexFileName);
         Git myGit1 = new Git();
+
         System.out.println("Case 1: Has Everything");
-        System.out.println("Expected Output: Git Directory, Objects Directory, and Index File DELETED.");
+        System.out.println("Expected Output: Git Directory, Objects Directory, and Index File ALL EXISTED.");
         System.out.print("Tester Output: ");
         deleteCurrent();
         System.out.println("");
 
-        System.out.println("Case 2: Has Git Direcotry with Nothing Inside");
+        System.out.println("Case 2: Has Git Directory with Nothing Inside");
         Git myGit2 = new Git();
         objectsDirectory.delete();
         indexFile.delete();
-        System.out.println("Expected Output: Git Directory DELETED. Objects Directory and Index File DID NOT EXIST.");
+        System.out.println("Expected Output: Git Directory EXISTED. Objects Directory and Index File DID NOT EXIST.");
         System.out.print("Tester Output: ");
         deleteCurrent();
         System.out.println("");
@@ -53,20 +54,20 @@ public class initTester {
         System.out.println("Case 3: Has Git Directory with No Objects Directory");
         Git myGit3 = new Git();
         objectsDirectory.delete();
-        System.out.println("Expected Output: Git Directory, and Index File DELETED. Objects Directory DID NOT EXIST.");
+        System.out.println("Expected Output: Git Directory, and Index File EXISTED. Objects Directory DID NOT EXIST.");
         System.out.print("Tester Output: ");
         deleteCurrent();
         System.out.println("");
 
-        System.out.println("Case 3: Has Git Directory with No Index File");
+        System.out.println("Case 4: Has Git Directory with No Index File");
         Git myGit4 = new Git();
         indexFile.delete();
-        System.out.println("Expected Output: Git Directory, and Objects Directory DELETED. Index File DID NOT EXIST.");
+        System.out.println("Expected Output: Git Directory, and Objects Directory EXISTED. Index File DID NOT EXIST.");
         System.out.print("Tester Output: ");
         deleteCurrent();
         System.out.println("");
 
-        System.out.println("Case 3: Has Nothing");
+        System.out.println("Case 5: Has Nothing");
         System.out.println("Expected Output: Git Directory, Objects Directory, and Index File ALL DID NOT EXIST");
         System.out.print("Tester Output: ");
         deleteCurrent();
