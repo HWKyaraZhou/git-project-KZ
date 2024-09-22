48ac8c498550db08f58c9b4f9e4fd26fb9c1f76a
Aviv Pilipski <apilipski1@hwemail.com>
2024-09-22 00:43:52 -0700

    Fixed Git Initialization Tester
    It was making a index directory instead of an index file!

--- Git.java
+++ Git.java
@@ -1,29 +1,29 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Git {
-    public Git () {
+    public Git () throws IOException {
         File gitDirectory = new File ("git");
         File objectsDirectory = new File ("git/objects");
-        File indexFile = new File ("git/index");
+        String indexFileName = "index";
+        File indexFile = new File (gitDirectory, indexFileName);
         if (!gitDirectory.exists()) {
             gitDirectory.mkdir();
             objectsDirectory.mkdirs();
-            indexFile.mkdirs();
+            indexFile.createNewFile();
         }
         else if (objectsDirectory.exists() && !indexFile.exists()){
-            indexFile.mkdirs();
+            indexFile.createNewFile();
         }
         else if (indexFile.exists() && !objectsDirectory.exists()) {
             objectsDirectory.mkdirs();
         }
         else if (!indexFile.exists() && !objectsDirectory.exists()) {
-            indexFile.mkdirs();
+            indexFile.createNewFile();
             objectsDirectory.mkdirs();
         }
         else {
@@ -31,25 +31,34 @@ public class Git {
         }
     }
     
-    public String hashingFunction (String fileName) throws FileNotFoundException {
-        File file = new File (fileName);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        return "";
-    }
-    public void fileWriter (String fileName) throws IOException {
-        String hash = hashingFunction(fileName);
-        File objectsDirectory = new File ("git/objects/hash");
-        objectsDirectory.mkdirs();
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-        BufferedWriter bw = new BufferedWriter(new FileWriter("git/objects/hash"));
-        while (br.ready()) {
-            bw.write(br.read());
+    public String hashingFunction(String fileName) throws FileNotFoundException, NoSuchAlgorithmException, IOException {
+        File file = new File(fileName);
+        MessageDigest digest = MessageDigest.getInstance("SHA-1");
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+            byte[] byteArray = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fis.read(byteArray)) != -1) {
+                digest.update(byteArray, 0, bytesRead);
+            }
         }
         
+        byte[] hash = digest.digest();
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
     }
 
-    public void makeBLOB (String fileName) throws FileNotFoundException {
-        fileWriter(fileName);
+    public void makeBLOB (String fileName) throws IOException, NoSuchAlgorithmException {
+        String hash = hashingFunction(fileName);
+        File newObject = new File ("git/objects/hash");
+        newObject.mkdirs();
     }
 
 }
